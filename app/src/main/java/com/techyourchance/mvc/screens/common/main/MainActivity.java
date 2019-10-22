763cd84d00bc85275b02700e8af9258f3655419f
ahmed96alaa <ahmed96zenhom@gmail.com>
2019-10-23 00:01:55 +0200

    migrating to android x and updating

--- app/src/main/java/com/techyourchance/mvc/screens/common/main/MainActivity.java
+++ app/src/main/java/com/techyourchance/mvc/screens/common/main/MainActivity.java
@@ -1,13 +1,10 @@
 package com.techyourchance.mvc.screens.common.main;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentTransaction;
+import androidx.annotation.Nullable;
+
 import android.widget.FrameLayout;
 
-import com.techyourchance.mvc.R;
 import com.techyourchance.mvc.screens.common.controllers.BackPressDispatcher;
 import com.techyourchance.mvc.screens.common.controllers.BackPressedListener;
 import com.techyourchance.mvc.screens.common.controllers.BaseActivity;
@@ -15,7 +12,6 @@ import com.techyourchance.mvc.screens.common.fragmentframehelper.FragmentFrameWr
 import com.techyourchance.mvc.screens.common.navdrawer.NavDrawerHelper;
 import com.techyourchance.mvc.screens.common.navdrawer.NavDrawerViewMvc;
 import com.techyourchance.mvc.screens.common.screensnavigator.ScreensNavigator;
-import com.techyourchance.mvc.screens.questionslist.QuestionsListFragment;
 
 import java.util.HashSet;
 import java.util.Set;
